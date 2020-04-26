2a79867fb324c442a722247048f75d246ddf1909
Mathew Sanders <mathew.sanders@gmail.com>
2020-04-25 22:24:26 -0400

    Update formatting for chart value axis

--- covid-19.playground/Sources/ChartView.swift
+++ covid-19.playground/Sources/ChartView.swift
@@ -57,18 +57,6 @@ extension ChartLayout {
     }
 }
 
-extension Double {
-    func string(maximumFractionDigits: Int = 2) -> String {
-        let s = String(format: "%.\(maximumFractionDigits)f", self)
-        for i in stride(from: 0, to: -maximumFractionDigits, by: -1) {
-            if s[s.index(s.endIndex, offsetBy: i - 1)] != "0" {
-                return String(s[..<s.index(s.endIndex, offsetBy: i)])
-            }
-        }
-        return String(s[..<s.index(s.endIndex, offsetBy: -maximumFractionDigits - 1)])
-    }
-}
-
 struct ChartValueAxis: ChartLayout, View {
     
     var valueGuides: [Double]
@@ -78,8 +66,14 @@ struct ChartValueAxis: ChartLayout, View {
         return valueGuides.max() ?? 0
     }
     
+    var numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
+    
     func shortValueLabel(for valueGuide: Double) -> String {
-        return valueGuide.string(maximumFractionDigits: 2)
+        return numberFormatter.string(from: NSNumber(value: valueGuide)) ?? ""
     }
     
     public var body: some View {
